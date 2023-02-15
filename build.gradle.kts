4051e5d2b04ab0fe56d5445469c5ee90d94f4d40
Peter Wu <piterwu@outlook.com>
2023-02-15 16:50:20 +0800

    仓库配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,6 @@ allprojects {
 
     repositories {
         mavenLocal()
-        maven("http://maven.wintruelife.com/nexus/content/repositories/public/") {
-            isAllowInsecureProtocol = true
-        }
         maven("https://maven.aliyun.com/repository/public/")
         maven("https://s01.oss.sonatype.org/content/groups/public/")
         mavenCentral()
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,9 +6,6 @@ configurations {
 
 repositories {
     mavenLocal()
-    maven("http://maven.wintruelife.com/nexus/content/repositories/public/") {
-        isAllowInsecureProtocol = true
-    }
     maven("https://maven.aliyun.com/repository/gradle-plugin/")
     maven("https://maven.aliyun.com/repository/public/")
     maven("https://s01.oss.sonatype.org/content/groups/public/")
